ecd516b39f2de8a2a72fa474b48ce643087ff44b
NeiloErnesto89 <neil.smyth1989@hotmail.com>
2023-03-11 21:40:25 +0000

    App currently function with members and bookings -> need to refine a model that takes time intervial

---
+++ GolfClubApp/Migrations/Booking/20230311213837_Update6.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GolfClubApp.Migrations.Booking
+{
+    /// <inheritdoc />
+    public partial class Update6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumMembers",
+                table: "Booking");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumMembers",
+                table: "Booking",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
