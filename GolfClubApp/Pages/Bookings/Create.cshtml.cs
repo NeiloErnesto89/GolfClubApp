a1e7db3b8f7c4e3ce978c7cf1977897b0b469a69
NeiloErnesto89 <neil.smyth1989@hotmail.com>
2023-03-14 21:50:37 +0000

    4 validations are working on the create booking and 2 on the edit - need to add the 2 if conditions into edit (duplication but oh well). Specific context check works for member and same date and now 4 booking times

--- GolfClubApp/Pages/Bookings/Create.cshtml.cs
+++ GolfClubApp/Pages/Bookings/Create.cshtml.cs
@@ -60,6 +60,17 @@ namespace GolfClubApp.Pages.Bookings
             // end of test
 
 
+            // test to ensure that only 4 players can book the same time (e.g john, joe, mary & sally (max) can play at 9.15am 
+
+            var existingBookings = await _context.Booking.Where(b => b.Time.Date == Booking.Time.Date && b.Time.TimeOfDay == Booking.Time.TimeOfDay).ToListAsync();
+            if (existingBookings.Count >= 4)
+            {
+                ModelState.AddModelError(string.Empty, "This time slot is already fully booked by 4 members");
+                ViewData["MemberId"] = new SelectList(_context.Member, "Id", "Name"); //reset member ID
+                return Page();
+            }
+            // end of test
+
             _context.Booking.Add(Booking);
             await _context.SaveChangesAsync();
 
